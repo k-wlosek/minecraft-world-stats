c5cccfb1b4ec9bec659ec131a260f6b040d3e321
kw <115936263+k-wlosek@users.noreply.github.com>
2023-04-01 15:44:14 +0200

    main.py - fixes for edge cases where player does not have stats/advancements or has invalid UUID analyse.py - caching for player heads

--- analyse.py
+++ analyse.py
@@ -7,6 +7,16 @@ import requests
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
 
 
+head_cache = {}
+
+
+def get_player_head(uuid: str) -> bytes:
+    if uuid not in head_cache:
+        head_cache[uuid] = requests.get(f'https://crafatar.com/avatars/{uuid}?size=32').content
+        print(f'Added {uuid} to head_cache')
+    return head_cache[uuid]
+
+
 def offset_image(coord: int, name: str, ax: plt.Axes, image: list) -> None:
     img = image
     im = OffsetImage(img, zoom=0.72)
@@ -16,17 +26,18 @@ def offset_image(coord: int, name: str, ax: plt.Axes, image: list) -> None:
     ax.add_artist(ab)
 
 
-def plot(player_list: list[tuple], values: list, name: str, title: str, xlabel: str, ylabel: str) -> None:
+def plot(player_list: list[tuple], values: list, name: str,
+         title: str, xlabel: str, ylabel: str, figsize: tuple[int, int]) -> None:
     players_names = [x[0] for x in player_list]
     skins = []
     for x in player_list:
-        img_data = requests.get(f'https://crafatar.com/avatars/{x[1]}?size=32').content
+        img_data = get_player_head(x[1])
         with open('tmp.jpg', 'wb') as handler:
             handler.write(img_data)
         im = image.imread('tmp.jpg')
         skins.append(im)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=figsize)
     bars = ax.bar(range(len(players_names)), values, width=0.5, align="center")
     ax.set_xticks(range(len(players_names)))
     ax.set_xticklabels(players_names)
@@ -77,7 +88,9 @@ if __name__ == "__main__":
     stat_fields = ['minecraft:play_time', 'minecraft:fish_caught', 'minecraft:sprint_one_cm', 'minecraft:walk_one_cm',
                    'minecraft:crouch_one_cm', 'minecraft:sneak_time', 'minecraft:swim_one_cm',
                    'minecraft:walk_under_water_one_cm', 'minecraft:walk_on_water_one_cm', 'minecraft:aviate_one_cm']
-    CSV_FILE = 'NBTTest_playerinfo.csv'
+    CSV_FILE = 'world_playerinfo.csv'
+    # Last arg of plot() is the size. Used below whenever you see plot are values for a large player base
+    # For png files we're saving, this is tuple is (width x 1000px, height x 1000px)
 
     players = []
     with open(CSV_FILE, newline='') as csvfile:
@@ -88,31 +101,40 @@ if __name__ == "__main__":
                 name, uuid, stats, completed_advancements = row[1], row[0], row[6], row[7]
                 json_acceptable_string = stats.replace("'", "\"")
                 stats_json = json.loads(json_acceptable_string)
-                general_stats = stats_json['minecraft:custom']
-                stat_values = []
-                for stat in stat_fields:
-                    try:
-                        stat_values.append(general_stats[stat])
-                    except KeyError:
-                        # Player has 0 of this stat - therefore error - key does not exist
+                try:
+                    general_stats = stats_json['minecraft:custom']
+                    stat_values = []
+                    for stat in stat_fields:
+                        try:
+                            stat_values.append(general_stats[stat])
+                        except KeyError:
+                            # Player has 0 of this stat - therefore error - key does not exist
+                            stat_values.append(0)
+                except KeyError:  # No general stats - fill with zeros
+                    stat_values = []
+                    for stat in stat_fields:
                         stat_values.append(0)
                 n_of_adv = len(completed_advancements.strip('[]').split(', '))
                 players.append((name, uuid, stat_values, n_of_adv))
+    print('Finished loading all playerdata')
 
     # Sort by x
     players_cpy = [x for x in players]
     players_cpy.sort(key=lambda x: x[3])  # Sort by n_of_adv
     players_cpy.reverse()  # Make sorting decreasing
     counts = [x[3] for x in players_cpy]
-    plot(players_cpy, counts, 'out/advancements.png', 'Advancements', 'Players', 'Advancement count')
+
+    print('Plotting all advancement')
+    plot(players_cpy, counts, 'out/advancements.png', 'Advancements', 'Players', 'Advancement count', (450, 10))
 
     # wanted_stat = 'minecraft:play_time'
     for stat in stat_fields:
         stat_name = stat.split(':')[1]
+        print(f'Plotting all {stat_name}')
         players_cpy.sort(key=lambda x: x[2][stat_fields.index(stat)])  # Sort by wanted stat
         players_cpy.reverse()  # Make sorting decreasing
         vals = [x[2][stat_fields.index(stat)] for x in players_cpy]
-        plot(players_cpy, vals, f'out/{stat_name}.png', stat_name, 'Players', 'Value')
+        plot(players_cpy, vals, f'out/{stat_name}.png', stat_name, 'Players', 'Value', (450, 10))
 
     # get top10 players
 
@@ -121,12 +143,16 @@ if __name__ == "__main__":
     players_cpy.reverse()  # Make sorting decreasing
     players_cpy = players_cpy[:10]
     counts = [x[3] for x in players_cpy]
-    plot(players_cpy, counts, 'out/top10/advancements_top10.png', 'Advancements', 'Players', 'Advancement count')
+
+    print('Plotting top10 advancements')
+    plot(players_cpy, counts, 'out/top10/advancements_top10.png',
+         'Advancements', 'Players', 'Advancement count', (14, 10))
     # wanted_stat = 'minecraft:play_time'
     for stat in stat_fields:
         stat_name = stat.split(':')[1]
+        print(f'Plotting top10 {stat_name}')
         players_cpy.sort(key=lambda x: x[2][stat_fields.index(stat)])  # Sort by wanted stat
         players_cpy.reverse()  # Make sorting decreasing
         players_cpy = players_cpy[:10]
         vals = [x[2][stat_fields.index(stat)] for x in players_cpy]
-        plot(players_cpy, vals, f'out/top10/{stat_name}_top10.png', stat_name, 'Players', 'Value')
+        plot(players_cpy, vals, f'out/top10/{stat_name}_top10.png', stat_name, 'Players', 'Value', (14, 10))
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import re
+
 from nbt import nbt
 import requests
 from base64 import b64decode
@@ -21,26 +23,38 @@ class Player:
         self.read_nbt()  # Populate private members
 
     def read_nbt(self):
-        self.__data = nbt.NBTFile(self.file, 'rb')
+        try:
+            self.__data = nbt.NBTFile(self.file, 'rb')
+        except nbt.MalformedFileError:
+            self.__data = None  # Failed to read NBT file
 
-    def get_player_base_info(self):
-        resp = requests.get(f'https://sessionserver.mojang.com/session/minecraft/profile/{self.uuid}').json()
+    def get_player_base_info(self) -> bool:
+        resp = requests.get(f'https://sessionserver.mojang.com/session/minecraft/profile/{self.uuid[:36]}').json()
+        # Cut off trailing numbers from UUID to make it valid
+        if 'errorMessage' in resp:
+            # API returned an error, such as invalid UUID
+            return False
         self.name = resp['name']
         nested = loads(b64decode(resp['properties'][0]['value']).decode(encoding='utf-8'))
         textures = nested['textures']
         self.skin = textures['SKIN']['url']
-        self.cape = textures['CAPE']['url']
+        try:
+            self.cape = textures['CAPE']['url']
+        except KeyError:
+            pass
+        return True
 
     def read_containers(self):
         """
         Reads player-tied containers, such as Inventory and Enderchest
         Both are saved as class properties in format [(item, count), (item, count)]
         """
-        for slot in [x.tags for x in self.__data['Inventory']]:
-            self.inventory.append((slot[1], slot[2].value))
-        self.enderchest = []
-        for slot in [x.tags for x in self.__data['EnderItems']]:
-            self.enderchest.append((slot[1], slot[2].value))
+        if self.__data:
+            for slot in [x.tags for x in self.__data['Inventory']]:
+                self.inventory.append((slot[1], slot[2].value))
+            self.enderchest = []
+            for slot in [x.tags for x in self.__data['EnderItems']]:
+                self.enderchest.append((slot[1], slot[2].value))
 
 
 class PlayerStats(Player):
@@ -51,13 +65,18 @@ class PlayerStats(Player):
 
     def read_json(self):
         head, tail = os.path.split(self.file)
-        # Climb up directory tree
-        with open(os.path.join(head.replace('playerdata', 'stats'), tail.replace('.dat', '.json')), 'r') as j:
-            self.__data = json.load(j)
+        try:
+            # Climb up directory tree
+            with open(os.path.join(head.replace('playerdata', 'stats'), tail.replace('.dat', '.json')), 'r') as j:
+                self.__data = json.load(j)
+        except FileNotFoundError:
+            self.__data = None  # No stats found for UUID
+            print(f'No stats for {self.name}, uuid: {self.uuid}')
 
     def get_stats(self):
-        # All stats as dict, ex. game_time is in ['minecraft:custom']['minecraft:total_world_time']
-        self.stats = self.__data['stats']
+        if self.__data:
+            # All stats as dict, ex. game_time is in ['minecraft:custom']['minecraft:total_world_time']
+            self.stats = self.__data['stats']
 
 
 class PlayerAdvancements(Player):
@@ -68,34 +87,90 @@ class PlayerAdvancements(Player):
 
     def read_json(self):
         head, tail = os.path.split(self.file)
-        with open(os.path.join(head.replace('playerdata', 'advancements'), tail.replace('.dat', '.json')), 'r') as j:
-            self.__data = json.load(j)
+        try:
+            with open(os.path.join(head.replace('playerdata', 'advancements'), tail.replace('.dat', '.json')), 'r') as j:
+                self.__data = json.load(j)
+        except FileNotFoundError:
+            self.__data = None  # Not found, no advancements probably
+            print(f'No advancements for {self.name}, uuid: {self.uuid}')
 
     def get_advancements(self):
-        for advancement, value in self.__data.items():
-            try:
-                if value['done']:
-                    self.completed.append(advancement)
-            except TypeError:
-                # Last field is int - version of file - ignore
-                pass
+        if self.__data:
+            for advancement, value in self.__data.items():
+                try:
+                    if value['done']:
+                        self.completed.append(advancement)
+                except TypeError:
+                    # Last field is int - version of file - ignore
+                    pass
 
 
 if __name__ == "__main__":
-    PLAYERDATA_DIR = 'NBTTest/playerdata/'
+    PLAYERDATA_DIR = 'world/playerdata/'
     world_name = PLAYERDATA_DIR.split('/')[0] or PLAYERDATA_DIR.split('\\')[0]
+    print(f'Output to {world_name}_playerinfo.csv')
+
+    # ChatGPT actually helped on this one, (the well documented portion) after 20min of back and forth
+    files = [f for f in os.listdir(PLAYERDATA_DIR) if f.endswith('.dat') and not f.endswith('_old.dat')]
+    players = []
+
+    for file in files:
+        if '-' in file:
+            base_name, ext = os.path.splitext(file)
+            uuid, nums = base_name.rsplit('-', 1)
+            try:
+                nums = int(nums)
+                base_file = f"{uuid}.dat"
+                if base_file not in files:
+                    continue
+                base_path = os.path.join(PLAYERDATA_DIR, base_file)
+                nums_path = os.path.join(PLAYERDATA_DIR, file)
+                if os.path.getsize(base_path) > os.path.getsize(nums_path):
+                    players.append(base_file)
+                else:
+                    players.append(file)
+                    files.remove(base_file)
+            except ValueError:
+                players.append(file)
+        else:
+            players.append(file)
+
+    # Remove duplicates
+    players = list(set(players))
+
+    # Add files with numbers if they have a larger size
+    for file in files:
+        if '-' in file:
+            base_name, ext = os.path.splitext(file)
+            uuid, nums = base_name.rsplit('-', 1)
+            try:
+                nums = int(nums)
+                base_file = f"{uuid}.dat"
+                if base_file in players:
+                    continue
+                base_path = os.path.join(PLAYERDATA_DIR, base_file)
+                nums_path = os.path.join(PLAYERDATA_DIR, file)
+                if os.path.getsize(nums_path) > os.path.getsize(base_path):
+                    players.append(file)
+            except ValueError:
+                pass
 
     with open(f'{world_name}_playerinfo.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # Write first row (col names)
         csvwriter.writerow(['uuid', 'name', 'skin', 'cape', 'inventory', 'enderchest', 'stats', 'completed_advancements'])
-        for file in os.listdir(PLAYERDATA_DIR):
+        for file in players:
             if file.endswith('.dat'):  # Possible backups in form of .dat_old, dismiss them
-                adv: PlayerAdvancements = PlayerAdvancements(file.replace('.dat', ''), PLAYERDATA_DIR + file)
-                adv.get_player_base_info()
+                uuid: str = file.replace('.dat', '')
+                adv: PlayerAdvancements = PlayerAdvancements(uuid, PLAYERDATA_DIR + file)
+                success = adv.get_player_base_info()
+                if not success:  # Do not write invalid uuids
+                    print(f"Reading for {adv.name} failed, problematic UUID: {file.replace('.dat', '')}")
+                    continue
+                print(f'Reading for {adv.name}')
                 adv.read_containers()
                 adv.get_advancements()
-                st: PlayerStats = PlayerStats(file.replace('.dat', ''), PLAYERDATA_DIR + file)
+                st: PlayerStats = PlayerStats(uuid, PLAYERDATA_DIR + file)
                 st.get_stats()
 
                 csvwriter.writerow([adv.uuid, adv.name, adv.skin, adv.cape,
